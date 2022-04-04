37074e7cd686733a4757e4c3e1ddbef5d08ad8d4
vince095 <vincent_thejane@outlook.com>
2022-04-04 22:24:47 +0200

    added backend

--- js/custom.js
+++ js/custom.js
@@ -24,6 +24,25 @@
       "images/slide/cosmo.jpg",
       "images/slide/cosmo2.jpg",
     ],  {duration: 2000, fade: 750});
+
+    //Post request
+    $('#submit').click(function(e){
+      $.ajax({
+        url: "localhost:4000/api/cosmotech/project",
+        type: "POST",
+        dataType: "json",
+        contentType: "application/json",
+        data: JSON.stringify({"name": $('#name').val(), "email": $('#email').val(), "info": $('#subject').val(), "description": $('#message').val()}),
+        processData: false,
+        success: function(data) {
+          console.log(data);
+        },
+        error: function(error) {
+          console.log(error);
+        }
+      })
+      e.preventDefault();
+    });
     
   })(window.jQuery);
 
